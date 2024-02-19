52a3cbad296dcb3e44b9eee57b2e73269b02dce6
Uncle10 <117525299+ThienLuong1101@users.noreply.github.com>
2024-02-19 20:42:59 +1030

    Delete oop-s1-2023-project/oop-s1-2023-project directory

--- oop-s1-2023-project/oop-s1-2023-project/latest/UnitTest.cpp
+++
@@ -1,7 +0,0 @@
-#include"UnitTest.h"
-
-// g++ UnitTest.cpp NewGame.cpp Player.cpp pokemon.cpp RandomPoke.cpp Stage1Pokemon.cpp Stage2Pokemon.cpp Battle.cpp PvP.cpp MasterPvP.cpp Opponent.cpp -o test
-int main() {
-    UnitTest test;
-    test.runTest();
-}
